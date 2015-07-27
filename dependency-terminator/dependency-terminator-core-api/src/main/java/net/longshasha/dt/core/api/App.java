b87698382e27e634961f73988a1b0fbac4923be5
陈雨清 <washmorec@olymtech.com>
2015-07-27 11:31:04 +0800

    ibatis映射文件生成器

---
+++ dependency-terminator/dependency-terminator-core-api/src/main/java/net/longshasha/dt/core/api/App.java
@@ -0,0 +1,13 @@
+package net.longshasha.dt.core.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
